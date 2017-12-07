5928fb54a602fc573f098a4516946adcc7dcde85
changwooPark <>
2017-12-07 15:22:44 +0900

    commit test

--- quickstart-master/src/app/app.component.spec.ts
+++ quickstart-master/src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+//atom svn commit test
 describe('AppComponent', function () {
   let de: DebugElement;
   let comp: AppComponent;
